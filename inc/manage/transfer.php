d1b4b8db37befb7def9fc8802f98b543c0ac4cc0
Kamta Rawlings <kamtarawlings@gmail.com>
2026-02-27 00:58:15 +0100

    added comments to the transfer.php file with name task pro

--- inc/manage/transfer.php
+++ inc/manage/transfer.php
@@ -1,8 +1,22 @@
+<!-- Header Section (Top bar of your page) -->
 <header class="header">
-		<h2 class="u-name">TASK <b>Pro</b>
-			<label for="checkbox">
-				<i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
-			</label>
-		</h2>
-		<i class="fa fa-bell" aria-hidden="true"></i>
-	</header>
+
+    <!-- Website/App Name -->
+    <h2 class="u-name">
+        TASK <b>Pro</b> <!-- "Pro" is bold for styling -->
+
+        <!-- Menu Toggle Button -->
+        <!-- This label is linked to a checkbox (used to open/close sidebar) -->
+        <label for="checkbox">
+            <!-- Hamburger icon (menu icon) -->
+            <i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
+        </label>
+    </h2>
+
+    <!-- Notification Icon Section -->
+    <div class="header-icons">
+        <!-- Bell icon for notifications -->
+        <i class="fa fa-bell" aria-hidden="true"></i>
+    </div>
+
+</header>
