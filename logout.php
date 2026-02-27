0abc6db8f9483d89e03a0736197f316172962188
Kamta Rawlings <kamtarawlings@gmail.com>
2026-02-27 01:13:21 +0100

    addded comments to the logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,28 @@
 <?php
-
+// Start the session
 session_start();
 
+// Unset all session variables
 session_unset();
+
+// Destroy the session completely
 session_destroy();
 
+// OPTIONAL: Destroy session cookie (extra security)
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000, // Expire cookie
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Redirect user to login page
 header("Location: login.php");
-    exit();
+exit();
+?>
