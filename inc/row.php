42a29449f2907bc0f7b1cb4e998f6598ffa2b0d5
Kamta Rawlings <kamtarawlings@gmail.com>
2026-02-27 01:05:15 +0100

    added comments on the row.php, adduser.php which is registration and also fixed codes

--- inc/row.php
+++ inc/row.php
@@ -1,86 +1,128 @@
+<?php
+// Start session (VERY IMPORTANT if not already started)
+session_start();
+
+// Optional safety check: redirect if not logged in
+if (!isset($_SESSION['username']) || !isset($_SESSION['role'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!-- Sidebar Navigation -->
 <nav class="side-bar">
-			<div class="user-p">
-				<img src="pic.png.jpg">
-				<h4>@<?=$_SESSION['username']?></h4>
-			</div>
-            
-			 <?php
-		
-			  if($_SESSION['role'] == "employee"){
-			  ?>
-			  <!-- Employee Navigation Bar -->
-			<ul>
-				<li>
-					<a href="#">
-						<i class="fa fa-tachometer" aria-hidden="true"></i>
-						<span>Dashboard</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<i class="fa fa-tasks" aria-hidden="true"></i>
-						<span>My Task</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<i class="fa fa-users" aria-hidden="true"></i>
-						<span>Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<i class="fa fa-bell" aria-hidden="true"></i>
-						<span>Notifications</span>
-					</a>
-				</li>
-				<li>
-					<a href="logout.php">
-						<i class="fa fa-sign-out" aria-hidden="true"></i>
-						<span>Logout</span>
-					</a>
-				</li>
-			</ul>
-			<?php
-			 }else { ?>
-			 <!-- Admin Navigation Bar -->
-			<ul id="navlist">
-				<li>
-					<a href="#">
-						<i class="fa fa-tachometer" aria-hidden="true"></i>
-						<span>Dashboard</span>
-					</a>
-				</li>
-				<li class="active">
-					<a href="user.php">
-						<i class="fa fa-users" aria-hidden="true"></i>
-						<span>Manage Users</span>
-					</a>
-				</li>
-				<li>
-					<a href="create_task.php">
-						<i class="fa fa-plus" aria-hidden="true"></i>
-						<span>Create Task</span>
-					</a>
-				</li>
-				<li>
-					<a href="tasks.php">
-						<i class="fa fa-tasks" aria-hidden="true"></i>
-						<span>All Tasks</span>
-					</a>
-				</li>
-				<li>
-					<a href="notification.php">
-						<i class="fa fa-bell" aria-hidden="true"></i>
-						<span>Notifications</span>
-					</a>
-				</li>
-				<li>
-					<a href="logout.php">
-						<i class="fa fa-sign-out" aria-hidden="true"></i>
-						<span>Logout</span>
-					</a>
-				</li>
-			</ul>
-			<?php }?>
-		</nav>
+
+    <!-- User Profile Section -->
+    <div class="user-p">
+        <!-- Profile Image -->
+        <img src="pic.png.jpg" alt="User Image">
+
+        <!-- Display logged-in username -->
+        <h4>@<?= htmlspecialchars($_SESSION['username']) ?></h4>
+    </div>
+
+    <?php if ($_SESSION['role'] == "employee") { ?>
+
+        <!-- ================= EMPLOYEE NAVIGATION ================= -->
+        <ul>
+
+            <!-- Dashboard -->
+            <li>
+                <a href="index.php">
+                    <i class="fa fa-tachometer"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <!-- My Tasks -->
+            <li>
+                <a href="my_task.php">
+                    <i class="fa fa-tasks"></i>
+                    <span>My Task</span>
+                </a>
+            </li>
+
+            <!-- Profile -->
+            <li>
+                <a href="profile.php">
+                    <i class="fa fa-user"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+
+            <!-- Notifications -->
+            <li>
+                <a href="notification.php">
+                    <i class="fa fa-bell"></i>
+                    <span>Notifications</span>
+                </a>
+            </li>
+
+            <!-- Logout -->
+            <li>
+                <a href="logout.php">
+                    <i class="fa fa-sign-out"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
+
+        </ul>
+
+    <?php } else { ?>
+
+        <!-- ================= ADMIN NAVIGATION ================= -->
+        <ul id="navlist">
+
+            <!-- Dashboard -->
+            <li>
+                <a href="index.php">
+                    <i class="fa fa-tachometer"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <!-- Manage Users -->
+            <li class="active">
+                <a href="user.php">
+                    <i class="fa fa-users"></i>
+                    <span>Manage Users</span>
+                </a>
+            </li>
+
+            <!-- Create Task -->
+            <li>
+                <a href="create_task.php">
+                    <i class="fa fa-plus"></i>
+                    <span>Create Task</span>
+                </a>
+            </li>
+
+            <!-- All Tasks -->
+            <li>
+                <a href="tasks.php">
+                    <i class="fa fa-tasks"></i>
+                    <span>All Tasks</span>
+                </a>
+            </li>
+
+            <!-- Notifications -->
+            <li>
+                <a href="notification.php">
+                    <i class="fa fa-bell"></i>
+                    <span>Notifications</span>
+                </a>
+            </li>
+
+            <!-- Logout -->
+            <li>
+                <a href="logout.php">
+                    <i class="fa fa-sign-out"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
+
+        </ul>
+
+    <?php } ?>
+
+</nav>
